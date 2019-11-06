5ba1281e1144286845c9e5991936d5ccd133868c
Porinita959 <56675346+Porinita959@users.noreply.github.com>
2019-11-06 10:03:38 -0800

    Add files via upload

---
+++ LoginRider.java
@@ -0,0 +1,14 @@
+package com.example.caresharecarpool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginRider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_rider);
+    }
+}
